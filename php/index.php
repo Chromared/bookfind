a51baffedba6fc667b7f8d8f6760c6161546cfc6
Alban Muller <intelligence-artificiel@outlook.fr>
2023-11-29 17:42:05 +0000

    Téléverser un nouveau fichier

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include 'includes/header.php'; ?>
+</head>
+<body>
+    <?php include 'includes/navbar.php'; ?>
+    <h1>Bienvenue sur BookFind ! Ici, empruntez les livres du C.D.I !</h1>
+    <form method="GET" class="form">
+        <input type="search" name="search" class="form-control" />
+        <button class="btn" type="submit">Rechercher</button>
+    </form>
+    
+</body>
+</html>
