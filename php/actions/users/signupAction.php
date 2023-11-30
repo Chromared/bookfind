9b5803054a81192fa1256a2dab872ab03ba2b788
MythUp <105284202+MythUp@users.noreply.github.com>
2023-11-30 16:41:15 +0000

    Téléverser un nouveau fichier

---
+++ php/actions/users/signupAction.php
@@ -0,0 +1,9 @@
+<?php
+    if (isset($_POST['name']) AND isset($_POST['firstname']) AND isset($_POST['password']) AND isset($_POST['confirm_password']) AND isset($_POST['card']) AND isset($_POST['classe'])){
+
+        if (isset($_POST['rules'])){
+
+        }
+
+    }
+?>
