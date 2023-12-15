9875e359e9e83f5d34eb9e3ec35e6eb4bdb16850
MythUp <105284202+MythUp@users.noreply.github.com>
2023-12-15 12:01:23 +0000

    Update file login.php

--- php/login.php
+++ php/login.php
@@ -9,6 +9,7 @@
 <?php include 'includes/navbar.php'; ?>
 <form method="POST" class="form">
     <label class="form-label">Numéro de carte : </label><input type="number" max="99999999" required="required" name="card" class="form-control" />
+    <label class="form-label">Mot de passe : </label><input type="number" max="99999999" required="required" name="password" class="form-control" />
 </form>
 </body>
 </html>
