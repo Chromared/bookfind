8eeda0baf9c974635c4c830ea9dff5a708f68864
Alban Muller <intelligence-artificiel@outlook.fr>
2023-12-18 11:42:40 +0000

    Update 2 files
    - /php/actions/users/loginAction.php
    - /php/login.php

--- php/actions/users/loginAction.php
+++ php/actions/users/loginAction.php
@@ -1,3 +1,7 @@
 <?php
-    
+    if (isset($_POST['validate'])) {
+    if (isset($_POST['card']) AND isset($_POST['password'])) {
+    if (empty($_POST['card']) AND empty())
+
+    }}
 ?>
--- php/login.php
+++ php/login.php
@@ -12,7 +12,7 @@
     <fieldset class="form-fieldset"><legend class="form-legend">Se connecter</legend>
     <label class="form-label">Numéro de carte : </label><input type="number" max="99999999" required="required" name="card" class="form-control" />
     <br /><label class="form-label">Mot de passe : </label><input type="number" max="99999999" required="required" name="password" class="form-control" />
-    <br /><input class="form-btn-submit" type="submit" value="Connexion !" /> <input type="reset" value="Réinitialiser le formulaire" class="form-btn-reset" />
+    <br /><input class="form-btn-submit" type="submit" name="validate" value="Connexion !" /> <input type="reset" value="Réinitialiser le formulaire" class="form-btn-reset" />
     </fieldset>
 </form>
 </p>
