dd0284aec5f821899e94220685fedabc1d58ea2a
MythUp <105284202+MythUp@users.noreply.github.com>
2023-12-18 11:44:26 +0000

    Update file loginAction.php

--- code/actions/users/loginAction.php
+++ code/actions/users/loginAction.php
@@ -1,7 +1,9 @@
 <?php
     if (isset($_POST['validate'])) {
     if (isset($_POST['card']) AND isset($_POST['password'])) {
-    if (empty($_POST['card']) AND empty())
+    if (empty($_POST['card']) AND empty($_POST['password'])) {
 
-    }}
+        
+
+    }}}
 ?>
