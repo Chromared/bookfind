c22f4b11f62224ffb30760e8c7f24bab158e5011
MythUp <105284202+MythUp@users.noreply.github.com>
2023-12-18 11:44:51 +0000

    Update file signup.php

--- code/signup.php
+++ code/signup.php
@@ -18,7 +18,7 @@
     <br /><label class="form-label">Numéro de carte : </label><input type="number" maxlength="8" max="99999999" required="required" name="card" class="form-control"/>
     <br /><label class="form-label">Classe : </label><select class="form-control" name="classe" required="required" ><option value="6B">6B</option><option value="5B">5B</option><option value="4B">4B</option><option value="3B">3B</option><option value="6R">6R</option><option value="5R">5R</option><option value="4R">4R</option><option value="3R">3R</option><option value="6J">6J</option><option value="5J">5J</option><option value="4J">4J</option><option value="3J">3J</option><option value="6V">6V</option><option value="5V">5V</option><option value="4V">4V</option><option value="3V">3V</option></select>
     <br /><label class="form-label">Je confirme avoir lu et accepté-e le <a href="rules.php" target="_blank">réglement</a> et les <a href="cu.php" target="_blank">conditions d'utilisations</a> : </label><input required="required" type="checkbox" name="rules-cu"/>
-    <br /><input class="form-btn-submit" type="submit" value="Inscription !" /> <input type="reset" value="Réinitialiser le formulaire" class="form-btn-reset" />
+    <br /><input class="form-btn-submit" type="submit" name="validate" value="Inscription !" /> <input type="reset" value="Réinitialiser le formulaire" class="form-btn-reset" />
     </p>
 </form>
 </body>
