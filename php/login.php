8e0704a18127a6af8cb9caf4f354b07fd6bddca5
MythUp <105284202+MythUp@users.noreply.github.com>
2023-12-18 11:08:47 +0000

    Update file login.php

--- php/login.php
+++ php/login.php
@@ -7,10 +7,14 @@
 </head>
 <body>
 <?php include 'includes/navbar.php'; ?>
+<p>
 <form method="POST" class="form">
+    <fieldset class="form-fieldset"><legend class="form-legend">Se connecter</legend>
     <label class="form-label">Numéro de carte : </label><input type="number" max="99999999" required="required" name="card" class="form-control" />
     <br /><label class="form-label">Mot de passe : </label><input type="number" max="99999999" required="required" name="password" class="form-control" />
     <br /><input class="form-btn-submit" type="submit" value="Connexion !" /> <input type="reset" value="Réinitialiser le formulaire" class="form-btn-reset" />
+    </fieldset>
 </form>
+</p>
 </body>
 </html>
