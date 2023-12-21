f581d0f3fbece31f954ffe7d92b05baee93bca0c
MythUp <105284202+MythUp@users.noreply.github.com>
2023-12-21 11:35:47 +0000

    Update file loginAction.php

--- code/actions/users/loginAction.php
+++ code/actions/users/loginAction.php
@@ -3,7 +3,18 @@
     if (isset($_POST['card']) AND isset($_POST['password'])) {
     if (empty($_POST['card']) AND empty($_POST['password'])) {
 
+        $card = $_POST['card'];
+        $password = crypt($_POST['password'], PASSWORD_DEFAULT);
+
+        $checkIfUserAlreadyExists = $bdd->prepare('SELECT carte FROM users WHERE carte = ?');
+        $checkIfUserAlreadyExists->execute(array($card));
+
+        if($checkIfUserAlreadyExists->rowCount() == 1){
+
+            $checkIfUserAlreadyExists = $bdd->prepare('SELECT mdp FROM users WHERE carte = ?');
+            $checkIfUserAlreadyExists->execute(array($password));
+
         
 
-    }}}
+    }}}}
 ?>
