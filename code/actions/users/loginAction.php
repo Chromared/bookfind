a9d6894a4c046a64a5365b110b7e7ad4fd1ec1b5
JiaN0w <232626523+JiaN0w@users.noreply.github.com>
2023-12-21 12:08:12 +0000

    Update file loginAction.php

--- code/actions/users/loginAction.php
+++ code/actions/users/loginAction.php
@@ -16,6 +16,23 @@
 
             if($password == $passwordVerify){
 
+                 //Récupérer les informations de l'utilisateur
+                $getInfosOfThisUserReq = $bdd->prepare('SELECT id, carte, nom, prenom, grade, classe FROM users WHERE carte = ?');
+                $getInfosOfThisUserReq->execute(array($card));
+                                                  $usersInfos = $getInfosOfThisUserReq->fetch();
+                
+                //Authentifier l'utilisateur sur le site et récupérer ses données dans des variables globales sessions
+                $_SESSION['auth'] = true;
+                $_SESSION['admin'] = false;
+                $_SESSION['id'] = $usersInfos['id'];
+                $_SESSION['lastname'] = $usersInfos['nom'];
+                $_SESSION['firstname'] = $usersInfos['prenom'];
+                $_SESSION['carte'] = $usersInfos['carte'];
+                $_SESSION['classe'] = $usersInfos['classe'];
+                $_SESSION['grade'] = $usersInfos['grade'];
+                
+
+
         
 
     }else{
