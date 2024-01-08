a652ce599a3ed4a9ab2bb5b60d851bd89b7d8d16
MythUp <105284202+MythUp@users.noreply.github.com>
2024-01-08 13:30:07 +0000

    Remplacer header.php

--- code/includes/header.php
+++ code/includes/header.php
@@ -1,3 +1,5 @@
 <title>BookFind</title>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 <link rel="icon" src="style/img/iconesite.ico" />
+<script src="https://kit.fontawesome.com/36a4c41322.js" crossorigin="anonymous"></script>
+
