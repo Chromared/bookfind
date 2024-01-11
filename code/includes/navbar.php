7f3018b89060542c184156dcf7db656e6618c432
MythUp <105284202+MythUp@users.noreply.github.com>
2024-01-11 12:10:21 +0000

    Update file navbar.php

--- code/includes/navbar.php
+++ code/includes/navbar.php
@@ -1,22 +1,22 @@
 <div class="navbar">
 <button class="nav-button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aribua-label="Toggle navigation"></button>
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <div class="title-nav"></
-        <p><a href="#">BookFind</a></p>
+    <ul><div class="title-nav"></
+        <p><li><a href="#">BookFind</a></li></p>
     </div>
     <div class="link-nav">
-        <p><a href="index.php">Accueil</a></p>
+        <p><li><a href="index.php">Accueil</a></li></p>
     </div>
     <?php if (!isset($_SESSION['auth'])) { ?>
     <div class="link-nav">
-        <p><a href="login.php">Se connecter</a></p>
+        <p><li><a href="login.php">Se connecter</a></li></p>
     </div>
     <div class="link-nav">
-        <p><a href="signup.php">S'inscrire</a></p>
+        <p><li><a href="signup.php">S'inscrire</a></li></p>
     </div>
     <?php } elseif (isset($_SESSION['auth'])) { ?>
     <div class="link-nav">
-        <p><a href="profil.php">Mon profil</a></p>
+        <p><li><a href="profil.php">Mon profil</a></li></p>
     </div>
     <div class="link-nav">
         <p><a href="">Mes emprunts</a></p>
@@ -30,4 +30,4 @@
                 <p><a href="">Gérer BookFind</a></p>
             </div>
     <?php }} ?>
-</div>
+</div></ul>
