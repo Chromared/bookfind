fd050803374a55024ac33c45ee2792a9dd64b679
MythUp <105284202+MythUp@users.noreply.github.com>
2024-01-11 17:42:27 +0000

    Remplacer login.php

--- code/login.php
+++ code/login.php
@@ -1,3 +1,4 @@
+<?php require 'actions/users/loginAction.php'; ?>
 <?php session_start() ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -9,7 +10,6 @@
 <body>
 <?php include 'includes/navbar.php'; ?>
 <?php include 'actions/database.php'; ?>
-
 <p>
 <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
 <form method="POST" class="form">
@@ -20,6 +20,5 @@
     </fieldset>
 </form>
 </p>
-<?php include 'actions/users/loginAction.php'; ?>
 </body>
 </html>
