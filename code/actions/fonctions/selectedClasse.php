eb0dceae91cee1b99b96013b07b9c915c7a7c857
MythUp <105284202+MythUp@users.noreply.github.com>
2024-01-29 17:41:02 +0000

    Téléverser un nouveau fichier

---
+++ code/actions/fonctions/selectedClasse.php
@@ -0,0 +1,40 @@
+<?php
+function Classe($classe) {
+    switch ($classe)
+    {
+    case '6B':
+    echo 'selected="selected"';
+    break;
+
+    case '6R':
+    echo 'selected="selected"';
+    break;
+
+    case '6J':
+    echo 'selected="selected"';
+    break;
+
+    case '6V':
+    echo 'selected="selected"';
+    break;
+
+    case '5B':
+    echo 'selected="selected"';
+    break;
+
+    case '5R':
+    echo 'selected="selected"';
+    break;
+
+    case '5J':
+    echo 'selected="selected"';
+    break;
+
+    case '5V':
+    echo 'selected="selected"';
+    break;
+
+    default:
+    echo '';
+    }
+}
