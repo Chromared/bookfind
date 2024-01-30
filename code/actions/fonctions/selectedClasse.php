a9023bc1009d610605fab0767864d024f2b0346c
MythUp <105284202+MythUp@users.noreply.github.com>
2024-01-30 17:14:38 +0000

    Remplacer selectedClasse.php

--- code/actions/fonctions/selectedClasse.php
+++ code/actions/fonctions/selectedClasse.php
@@ -1,39 +1,104 @@
 <?php
-function Classe($classe) {
+function Classe($classe, $usersclasse) {
     switch ($classe)
     {
     case '6B':
-    echo 'selected="selected"';
+        if($usersclasse = '6B'){
+            echo'selected="selected"';
+        }
     break;
 
     case '6R':
-    echo 'selected="selected"';
+        if($usersclasse = '6R'){
+            echo'selected="selected"';
+        }
     break;
 
     case '6J':
-    echo 'selected="selected"';
+        if($usersclasse = '6J'){
+            echo'selected="selected"';
+        }
     break;
 
     case '6V':
-    echo 'selected="selected"';
+        if($usersclasse = '6V'){
+            echo'selected="selected"';
+        }
     break;
 
     case '5B':
-    echo 'selected="selected"';
+        if($usersclasse = '5B'){
+            echo'selected="selected"';
+        }
     break;
 
     case '5R':
-    echo 'selected="selected"';
+        if($usersclasse = '5R'){
+            echo'selected="selected"';
+        }
     break;
 
     case '5J':
-    echo 'selected="selected"';
+        if($usersclasse = '5J'){
+            echo'selected="selected"';
+        }
     break;
 
     case '5V':
-    echo 'selected="selected"';
+        if($usersclasse = '5V'){
+            echo'selected="selected"';
+        }
     break;
 
+    case '4B':
+        if($usersclasse = '4B'){
+            echo'selected="selected"';
+        }
+    break;
+
+    case '4R':
+        if($usersclasse = '4R'){
+            echo'selected="selected"';
+        }
+    break;
+
+    case '4J':
+        if($usersclasse = '4J'){
+            echo'selected="selected"';
+        }
+    break;
+
+    case '4V':
+        if($usersclasse = '4V'){
+            echo'selected="selected"';
+        }
+    break;
+
+    case '3B':
+        if($usersclasse = '3B'){
+            echo'selected="selected"';
+        }
+    break;
+
+    case '3R':
+        if($usersclasse = '3R'){
+            echo'selected="selected"';
+        }
+    break;
+
+    case '3J':
+        if($usersclasse = '3J'){
+            echo'selected="selected"';
+        }
+    break;
+
+    case '3V':
+        if($usersclasse = '3V'){
+            echo'selected="selected"';
+        }
+    break;
+
+            
     default:
     echo '';
     }
