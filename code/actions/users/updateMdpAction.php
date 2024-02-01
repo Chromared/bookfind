4e9620b8047debe58c2022ca23d04f4395a06868
MythUp <105284202+MythUp@users.noreply.github.com>
2024-02-01 17:06:42 +0000

    Update file updateMdpAction.php

--- code/actions/users/updateMdpAction.php
+++ code/actions/users/updateMdpAction.php
@@ -1,6 +1,22 @@
 <?php if (isset($_POST['validateMdp'])) {
     if (isset($_POST['actual-password']) AND isset($_POST['new-password']) AND isset($_POST['confirm-new-password'])) {
     if (!empty($_POST['actual-password']) AND !empty($_POST['new-password']) AND !empty($_POST['confirm-new-password'])) {
+
+        if ($_POST[new-password] == $_POST[confirm-new-password]) {
+            
+        $password = crypt($_POST['actual-password'], PASSWORD_DEFAULT);
+        $newPassword = crypt($_POST['new-password'], PASSWORD_DEFAULT);
+        $id = $_SESSION['id'];
+
+        $checkPassword = $bdd->prepare('SELECT mdp FROM users WHERE id = ?');
+        $checkPassword->execute(array($id));
+
+        if (password_verify($password, $checkPassword['mdp'])) {
+
+            $updateMdp = $bdd->prepare('UPDATE users SET mdp = ? WHERE id = ?');
+            $updateMdp->execute(array($newPassword, $id));
+
+    }
 }
 }
 }
