dd4179dd670d408629b66348600bfc124b2cd63e
MythUp <105284202+MythUp@users.noreply.github.com>
2024-02-01 17:00:41 +0000

    Remplacer updateInfoScoAction.php

--- code/actions/users/updateInfoScoAction.php
+++ code/actions/users/updateInfoScoAction.php
@@ -1,5 +1,5 @@
 <?php if (isset($_POST['validateInfoSco'])) {
-    if (isset($_POST['card']) AND iset($_POST['classe'])){
+    if (isset($_POST['card']) AND isset($_POST['classe'])){
     if (!empty($_POST['card']) AND !empty($_POST['classe'])){
         $card = htmlspecialchars($_POST['card']);
         $classe = htmlspecialchars($_POST['classe']);
@@ -8,7 +8,7 @@
         $updateInfoSco = $bdd->prepare('UPDATE users SET carte = ?, classe = ? WHERE id = ?');
         $updateInfoSco->execute(array($card, $classe, $id));
 
-        header('Location: updateProfil.php');
+        header('Location: updateProfil.php?id=' . $id .'');
     }else{
         $errorMsg = 'Veuillez compléter tous les champs';
     }
