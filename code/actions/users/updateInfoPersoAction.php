93b0918867ad2e94163f0995020cc943c9dc516d
MythUp <105284202+MythUp@users.noreply.github.com>
2024-02-01 11:44:42 +0000

    Update file updateInfoPersoAction.php

--- code/actions/users/updateInfoPersoAction.php
+++ code/actions/users/updateInfoPersoAction.php
@@ -1,6 +1,14 @@
 <?php
 if (isset($_POST['validateInfoPerso'])) {
         if (isset($_POST['name']) AND isset($_POST['firstname'])) {
+        if (!empty($_POST['name']) AND !empty($_POST['firstname'])) {
+            $name = htmlspecialchars($_POST['name']);
+            $firstname = htmlspecialchars($_POST['firstname']);
+            $id = $_SESSION['id'];
+
+            $updateInfoPerso = $bdd->prepare('UPDATE users SET name = ?, firstname = ? WHERE id = ?');
+            $updateInfoPerso->execute(array($name, $firstname, $id));
+            header('Location: ../index.php');
     }
 }
 ?>
