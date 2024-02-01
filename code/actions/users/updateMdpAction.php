bcd74e9a6ad8c9e336f0f523caa6008a1fdde8cf
MythUp <105284202+MythUp@users.noreply.github.com>
2024-02-01 17:00:57 +0000

    Remplacer updateMdpAction.php

---
+++ code/actions/users/updateMdpAction.php
@@ -0,0 +1,6 @@
+<?php if (isset($_POST['validateMdp'])) {
+    if (isset($_POST['actual-password']) AND isset($_POST['new-password']) AND isset($_POST['confirm-new-password'])) {
+    if (!empty($_POST['actual-password']) AND !empty($_POST['new-password']) AND !empty($_POST['confirm-new-password'])) {
+}
+}
+}
