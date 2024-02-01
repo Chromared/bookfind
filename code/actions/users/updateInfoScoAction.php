527405d3a4c5cd18b6916fd51fabcc417db36efc
Alban Muller <intelligence-artificiel@outlook.fr>
2024-02-01 16:39:45 +0000

    Update file updateInfoScoAction.php

---
+++ code/actions/users/updateInfoScoAction.php
@@ -0,0 +1,16 @@
+<?php if (isset($_POST['validateInfoSco'])) {
+    if (isset($_POST['card']) AND iset($_POST['classe'])){
+    if (!empty($_POST['card']) AND !empty($_POST['classe'])){
+        $card = htmlspecialchars($_POST['card']);
+        $classe = htmlspecialchars($_POST['classe']);
+        $id = $_SESSION['id'];
+
+        $updateInfoSco = $bdd->prepare('UPDATE users SET carte = ?, classe = ? WHERE id = ?');
+        $updateInfoSco->execute(array($card, $classe, $id));
+
+        header('Location: updateProfil.php');
+    }else{
+        $errorMsg = 'Veuillez compléter tous les champs';
+    }
+}
+}
