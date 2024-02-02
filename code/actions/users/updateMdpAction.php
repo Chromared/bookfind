ee839384b48508b16b0445aab685bd86648b090a
Alban Muller <intelligence-artificiel@outlook.fr>
2024-02-02 17:06:34 +0000

    Remplacer updateMdpAction.php

--- code/actions/users/updateMdpAction.php
+++ code/actions/users/updateMdpAction.php
@@ -2,7 +2,7 @@
     if (isset($_POST['actual-password']) AND isset($_POST['new-password']) AND isset($_POST['confirm-new-password'])) {
     if (!empty($_POST['actual-password']) AND !empty($_POST['new-password']) AND !empty($_POST['confirm-new-password'])) {
 
-        if ($_POST[new-password] == $_POST[confirm-new-password]) {
+        if ($_POST['new-password'] == $_POST['confirm-new-password']) {
             
         $password = crypt($_POST['actual-password'], PASSWORD_DEFAULT);
         $newPassword = crypt($_POST['new-password'], PASSWORD_DEFAULT);
@@ -11,12 +11,25 @@
         $checkPassword = $bdd->prepare('SELECT mdp FROM users WHERE id = ?');
         $checkPassword->execute(array($id));
 
-        if (password_verify($password, $checkPassword['mdp'])) {
+        $checkPassword2 = $checkPassword->fetch();
+
+        if (password_verify($password, $checkPassword2['mdp'])) {
 
             $updateMdp = $bdd->prepare('UPDATE users SET mdp = ? WHERE id = ?');
             $updateMdp->execute(array($newPassword, $id));
 
-    }
+            header('Location: updateProfil.php?m=1');
+
+    }else{
+        $errorMsg = '';
 }
+}else{
+    $errorMsg = '<div class="msg"><div class="msg-alerte">Les deux nouveaux mot de passe ne sont pas identiques.</div></div>';
+}
+}else{
+    $errorMsg = '<div class="msg"><div class="msg-alerte">Veuillez remplir tous les champs.</div></div>';
+}
+}else{
+    $errorMsg = '<div class="msg"><div class="msg-alerte">Tous les champs n\'existent pas. Veuillez <a href="updateProfil.php">recharger</a> la page.</div></div>';
 }
 }
