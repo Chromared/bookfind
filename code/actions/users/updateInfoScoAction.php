3b41c6b0aa5d0423a9f5d7cf188dec78bf0ca4e5
Alban Muller <intelligence-artificiel@outlook.fr>
2024-02-02 17:06:51 +0000

    Remplacer updateInfoScoAction.php

--- code/actions/users/updateInfoScoAction.php
+++ code/actions/users/updateInfoScoAction.php
@@ -1,6 +1,7 @@
 <?php if (isset($_POST['validateInfoSco'])) {
     if (isset($_POST['card']) AND isset($_POST['classe'])){
     if (!empty($_POST['card']) AND !empty($_POST['classe'])){
+        
         $card = htmlspecialchars($_POST['card']);
         $classe = htmlspecialchars($_POST['classe']);
         $id = $_SESSION['id'];
@@ -10,7 +11,9 @@
 
         header('Location: updateProfil.php?id=' . $id .'');
     }else{
-        $errorMsg = 'Veuillez compléter tous les champs';
+        $errorMsg = '<div class="msg"><div class="msg-alerte">Veuillez remplir tous les champs.</div></div>';
     }
+}else{
+    $errorMsg = '<div class="msg"><div class="msg-alerte">Tous les champs n\'existent pas. Veuillez <a href="updateProfil.php">recharger</a> la page.</div></div>';
 }
 }
