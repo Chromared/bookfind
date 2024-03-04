817f9ee8b2815c61b00e6c8391e1ccc9f722c632
Alban Muller <intelligence-artificiel@outlook.fr>
2024-03-04 16:53:20 +0000

    Remplacer login.php

--- code/login.php
+++ code/login.php
@@ -14,8 +14,8 @@
 <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
 <form method="POST" class="form">
     <fieldset class="form-fieldset"><legend class="form-legend">Se connecter</legend>
-    <label class="form-label">Numéro de carte : </label><input type="number" max="99999999" required="required" name="card" class="form-control" />
-    <br /><label class="form-label">Mot de passe : </label><input type="password" required="required" name="password" class="form-control" />
+    <label class="form-label">Numéro de carte : </label><input type="number" max="99999999" name="card" class="form-control" />
+    <br /><label class="form-label">Mot de passe : </label><input type="password" name="password" class="form-control" />
     <br /><input class="form-btn-blue" type="submit" name="validate" value="Connexion !" /> <input type="reset" value="Réinitialiser le formulaire" class="form-btn-blue" />
     </fieldset>
 </form>
