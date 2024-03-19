05596ef51db71a587960788ceb1c5257fdc89e2f
Alban Muller <intelligence-artificiel@outlook.fr>
2024-03-19 17:53:42 +0000

    Remplacer sUsers.php

--- code/gestion/actions/users/sUsers.php
+++ code/gestion/actions/users/sUsers.php
@@ -82,4 +82,22 @@
         <br />
 
 <?php }
+        if($s == 'Administrateur'){$s = '1';}elseif($s == 'Gérant(e) du C.D.I'){$s = '2';}elseif($s == 'Assistant(e) du C.D.I'){$s = '3';}elseif($s == 'Aucun'){$s = 0;}
+
+        $checkIfUserExists = $bdd->prepare('SELECT * FROM users WHERE grade = ? ORDER BY id');
+        $checkIfUserExists->execute(array($s));
+
+        echo '<h3 id="firstname">Par grade :</h3>';
+        while($users = $checkIfUserExists->fetch()){ ?>
+        <div class="bordure">
+            <h4><?= $users['prenom'] ?> <?= $users['nom'] ?></h4>
+            <p>ID : <?= $users['id'] ?></p>
+            <p>Carte : <?= $users['carte'] ?></p>
+            <p>Classe : <?= $users['classe'] ?></p>
+            <p>Grade : <?php Grade($users['grade']); ?><br /></p>
+            <p><a style="color: black;" href="modify-user.php?id=<?= $users['id'] ?>">Modifier l'utilisateur</a></p>
+        </div>
+        <br />
+
+<?php }
 }elseif(!isset($_GET['s']) OR empty($_GET['s'])){}
