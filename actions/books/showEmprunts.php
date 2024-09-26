55c5ceaf5027c720884d58c6df7483c394f158f8
Alban Muller <intelligence-artificiel@outlook.fr>
2024-09-26 15:33:54 +0000

    Update 74 files
    - /actions/books/sBooks.php
    - /actions/books/showEmprunts.php
    - /actions/books/showOneBooksAction.php
    - /actions/fonctions/selectedClasse.php
    - /actions/fonctions/selectionnerClasse.php
    - /actions/fonctions/selectionnerGrade.php
    - /actions/fonctions/tradDateJourEnVersFr.php
    - /actions/fonctions/transDateMoisIntVersLettre.php
    - /actions/fonctions/transfoGradeIntVersText.php
    - /actions/users/deleteAccountAction1.php
    - /actions/users/deleteAccountAction2.php
    - /actions/users/loginAction.php
    - /actions/users/logoutAction.php
    - /actions/users/securityAction.php
    - /actions/users/securityGetIdProfil.php
    - /actions/users/showOneUsersProfilAction.php
    - /actions/users/signupAction.php
    - /actions/users/updateInfoPersoAction.php
    - /actions/users/updateInfoScoAction.php
    - /actions/users/updateMdpAction.php
    - /actions/bookfind.sql
    - /actions/database.php
    - /gestion/includes/navbar.php
    - /gestion/actions/books/addAuthorsAction.php
    - /gestion/actions/books/addBooksAction.php
    - /gestion/actions/books/addEmpruntAction.php
    - /gestion/actions/books/list-author.php
    - /gestion/actions/books/list-editeur.php
    - /gestion/actions/books/list-genre.php
    - /gestion/actions/books/list-type.php
    - /gestion/actions/fonctions/count_data.php
    - /gestion/actions/users/deleteAccountAction1.php
    - /gestion/actions/users/deleteAccountAction2.php
    - /gestion/actions/users/sUsers.php
    - /gestion/actions/users/updateGradeAction.php
    - /gestion/actions/users/updateInfoPersoAction.php
    - /gestion/actions/users/updateInfoScoAction.php
    - /gestion/actions/users/updateMdpAction.php
    - /gestion/actions/securityActionAdmin.php
    - /gestion/add-books.php
    - /gestion/add-emprunt.php
    - /gestion/books-reader.php
    - /gestion/books.php
    - /gestion/config-ht.php
    - /gestion/index.php
    - /gestion/log.php
    - /gestion/modify-user.php
    - /gestion/search-authors.php
    - /gestion/search-editors.php
    - /gestion/search-genres.php
    - /gestion/search-types.php
    - /gestion/update-user.php
    - /gestion/users.php
    - /includes/foot.php
    - /includes/header.php
    - /includes/navbar.php
    - /style/img/.gitkeep
    - /style/img/close_FILL0_wght400_GRAD0_opsz24.svg
    - /style/img/iconesite.ico
    - /style/img/iconesite.png
    - /style/img/logopourpage.png
    - /style/img/menu_FILL0_wght400_GRAD0_opsz24.svg
    - /style/palette_de_couleur.jpg
    - /style/style0.css
    - /books-reader.php
    - /books.php
    - /emprunts.php
    - /index.php
    - /login.php
    - /pdc.php
    - /profil.php
    - /rules.php
    - /signup.php
    - /updateProfil.php

--- actions/books/showEmprunts.php
+++
@@ -1,5 +0,0 @@
-<?php $card = htmlspecialchars($_GET['card']);
-      $selectInfosFromEmprunts= $bdd->prepare('SELECT * FROM emprunts WHERE card_emprunteur = ?');
-      $selectInfosFromEmprunts->execute(array($card));
-
-      $empruntsInfos = $selectInfosFromEmprunts->fetch();
--- actions/users/deleteAccountAction2.php
+++
@@ -1,8 +0,0 @@
-<?php if (isset($_POST['validateDelete2'])){
-    if(isset($_POST['confirm-delete'])){
-    if(!empty($_POST['confirm-delete'])){
-
-        
-}
-}
-}
