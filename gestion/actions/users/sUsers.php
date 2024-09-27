476f0078d1b68e516a296d0ba809a1a80ebc68e3
MythUp <105284202+MythUp@users.noreply.github.com>
2024-09-27 16:32:34 +0000

    Update 3 files
    - /actions/books/showEmprunts.php
    - /gestion/actions/users/sUsers.php
    - /gestion/users.php

--- gestion/actions/users/sUsers.php
+++ gestion/actions/users/sUsers.php
@@ -1,108 +1,41 @@
-<?php include '../actions/fonctions/transfoGradeIntVersText.php'; ?>
-<?php if(isset($_GET['s']) AND !empty($_GET['s'])){
-        $s = $_GET['s'];
+<?php include '../actions/fonctions/transfoGradeIntVersText.php';
 
-            $checkIfUserExists = $bdd->prepare('SELECT * FROM users WHERE id = ? ORDER BY id');
-            $checkIfUserExists->execute(array($s));
+// Liste des colonnes valides pour la recherche
+$colonnes_valides = ['id', 'carte', 'classe', 'nom', 'prenom', 'mdp', 'grade', 'datetime', 'regles', 'pdc', 'nb_emprunt_max', 'nb_emprunt'];
 
-            echo '<h3 id="id">Par ID :</h3>';
-            while($users = $checkIfUserExists->fetch()){ ?>
+if (isset($_GET['s']) && !empty($_GET['s']) && isset($_GET['where']) && !empty($_GET['where'])) {
+    
+    // Échapper les données d'entrée utilisateur
+    $s = htmlspecialchars($_GET['s']);
+    $where = htmlspecialchars($_GET['where']);
+    
+    // Valider que la colonne demandée est dans la liste des colonnes autorisées
+    if (in_array($where, $colonnes_valides)) {
+        
+        // Préparer la requête SQL sécurisée
+        $requete = "SELECT * FROM users WHERE $where = :value ORDER BY $where ASC";
+        $SearchUser = $bdd->prepare($requete);
+        
+        // Lier la valeur de la recherche
+        $SearchUser->bindParam(':value', $s, PDO::PARAM_STR);
+        $SearchUser->execute();
+        
+        // Afficher les résultats
+        while ($users = $SearchUser->fetch()) { ?>
             <div class="bordure">
                 <h4><?= htmlspecialchars($users['prenom']); ?> <?= htmlspecialchars($users['nom']); ?></h4>
                 <p>ID : <?= htmlspecialchars($users['id']); ?></p>
                 <p>Carte : <?= htmlspecialchars($users['carte']); ?></p>
                 <p>Classe : <?= htmlspecialchars($users['classe']); ?></p>
-                <p>Nombre d'emprunt(s) : <?php echo '' . $users['nb_emprunt'] . '/' . $users['nb_emprunt_max'] . ''; ?></p>
-                <p>Grade : <?php Grade($users['grade']); ?><br /></p>
+                <p>Nombre d'emprunt(s) : <?= htmlspecialchars($users['nb_emprunt']) . '/' . htmlspecialchars($users['nb_emprunt_max']); ?></p>
+                <p>Grade : <?= Grade($users['grade']); ?><br /></p>
                 <p><a style="color: black;" href="update-user.php?id=<?= htmlspecialchars($users['id']); ?>">Modifier l'utilisateur</a></p>
             </div>
             <br />
-            
-<?php }
-            $checkIfUserExists = $bdd->prepare('SELECT * FROM users WHERE nom = ? ORDER BY id');
-            $checkIfUserExists->execute(array($s));
-
-            echo '<h3 id="name">Par nom :</h3>';
-            while($users = $checkIfUserExists->fetch()){ ?>
-            <div class="bordure">
-                <h4><?= htmlspecialchars($users['prenom']); ?> <?= htmlspecialchars($users['nom']); ?></h4>
-                <p>ID : <?= htmlspecialchars($users['id']); ?></p>
-                <p>Carte : <?= htmlspecialchars($users['carte']); ?></p>
-                <p>Classe : <?= htmlspecialchars($users['classe']); ?></p>
-                <p>Nombre d'emprunt(s) : <?php echo '' . $users['nb_emprunt'] . '/' . $users['nb_emprunt_max'] . ''; ?></p>
-                <p>Grade : <?php Grade($users['grade']); ?><br /></p>
-                <p><a style="color: black;" href="update-user.php?id=<?= htmlspecialchars($users['id']); ?>">Modifier l'utilisateur</a></p>
-            </div>
-            <br />
-            
-<?php }
-            $checkIfUserExists = $bdd->prepare('SELECT * FROM users WHERE prenom = ? ORDER BY id');
-            $checkIfUserExists->execute(array($s));
-
-            echo '<h3 id="firstname">Par prénom :</h3>';
-            while($users = $checkIfUserExists->fetch()){ ?>
-            <div class="bordure">
-                <h4><?= htmlspecialchars($users['prenom']); ?> <?= htmlspecialchars($users['nom']); ?></h4>
-                <p>ID : <?= htmlspecialchars($users['id']); ?></p>
-                <p>Carte : <?= htmlspecialchars($users['carte']); ?></p>
-                <p>Classe : <?= htmlspecialchars($users['classe']); ?></p>
-                <p>Nombre d'emprunt(s) : <?php echo '' . $users['nb_emprunt'] . '/' . $users['nb_emprunt_max'] . ''; ?></p>
-                <p>Grade : <?php Grade($users['grade']); ?><br /></p>
-                <p><a style="color: black;" href="update-user.php?id=<?= htmlspecialchars($users['id']); ?>">Modifier l'utilisateur</a></p>
-            </div>
-            <br />
-            
-<?php }
-            $checkIfUserExists = $bdd->prepare('SELECT * FROM users WHERE carte = ? ORDER BY id');
-            $checkIfUserExists->execute(array($s));
-
-            echo '<h3 id="firstname">Par carte :</h3>';
-            while($users = $checkIfUserExists->fetch()){ ?>
-            <div class="bordure">
-                <h4><?= htmlspecialchars($users['prenom']); ?> <?= htmlspecialchars($users['nom']); ?></h4>
-                <p>ID : <?= htmlspecialchars($users['id']); ?></p>
-                <p>Carte : <?= htmlspecialchars($users['carte']); ?></p>
-                <p>Classe : <?= htmlspecialchars($users['classe']); ?></p>
-                <p>Nombre d'emprunt(s) : <?php echo '' . $users['nb_emprunt'] . '/' . $users['nb_emprunt_max'] . ''; ?></p>
-                <p>Grade : <?php Grade($users['grade']); ?><br /></p>
-                <p><a style="color: black;" href="update-user.php?id=<?= htmlspecialchars($users['id']); ?>">Modifier l'utilisateur</a></p>
-            </div>
-            <br />
-
-<?php }
-        $checkIfUserExists = $bdd->prepare('SELECT * FROM users WHERE classe = ? ORDER BY id');
-        $checkIfUserExists->execute(array($s));
-
-        echo '<h3 id="firstname">Par classe :</h3>';
-        while($users = $checkIfUserExists->fetch()){ ?>
-        <div class="bordure">
-            <h4><?= htmlspecialchars($users['prenom']); ?> <?= htmlspecialchars($users['nom']); ?></h4>
-            <p>ID : <?= htmlspecialchars($users['id']); ?></p>
-            <p>Carte : <?= htmlspecialchars($users['carte']); ?></p>
-            <p>Classe : <?= htmlspecialchars($users['classe']); ?></p>
-            <p>Nombre d'emprunt(s) : <?php echo '' . $users['nb_emprunt'] . '/' . $users['nb_emprunt_max'] . ''; ?></p>
-            <p>Grade : <?php Grade($users['grade']); ?><br /></p>
-            <p><a style="color: black;" href="update-user.php?id=<?= htmlspecialchars($users['id']); ?>">Modifier l'utilisateur</a></p>
-        </div>
-        <br />
-
-<?php }
-        if($s == 'Administrateur'){$s = '1';}elseif($s == 'Gérant'){$s = '2';}elseif($s == 'Assistant'){$s = '3';}elseif($s == 'Aucun'){$s = 0;}
-
-        $checkIfUserExists = $bdd->prepare('SELECT * FROM users WHERE grade = ? ORDER BY id');
-        $checkIfUserExists->execute(array($s));
-
-        echo '<h3 id="firstname">Par grade :</h3>';
-        while($users = $checkIfUserExists->fetch()){ ?>
-        <div class="bordure">
-            <h4><?= htmlspecialchars($users['prenom']); ?> <?= htmlspecialchars($users['nom']); ?></h4>
-            <p>ID : <?= htmlspecialchars($users['id']); ?></p>
-            <p>Carte : <?= htmlspecialchars($users['carte']); ?></p>
-            <p>Classe : <?= htmlspecialchars($users['classe']); ?></p>
-            <p>Nombre d'emprunt(s) : <?php echo '' . $users['nb_emprunt'] . '/' . $users['nb_emprunt_max'] . ''; ?></p>
-            <p>Grade : <?php Grade($users['grade']); ?><br /></p>
-            <p><a style="color: black;" href="update-user.php?id=<?= htmlspecialchars($users['id']); ?>">Modifier l'utilisateur</a></p>
-        </div>
-        <br />
-
-<?php }}
+        <?php }
+        
+    } else {
+        echo "Erreur : Colonne non valide.";
+    }
+    
+}
