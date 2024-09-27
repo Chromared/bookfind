9cd9255361a34a4ab813980a2ee626f2144bef75
Alban Muller <intelligence-artificiel@outlook.fr>
2024-09-27 16:32:34 +0000

    Update 3 files
    - /actions/books/showEmprunts.php
    - /gestion/actions/users/sUsers.php
    - /gestion/users.php

--- actions/books/showEmprunts.php
+++ actions/books/showEmprunts.php
@@ -1,3 +1,3 @@
 <?php $card = htmlspecialchars($_GET['card']);
-      $selectInfosFromEmprunts= $bdd->prepare('SELECT * FROM emprunts WHERE card_emprunteur = ?');
+      $selectInfosFromEmprunts= $bdd->prepare('SELECT * FROM emprunts WHERE card_emprunteur = ? ORDER BY date_retour');
       $selectInfosFromEmprunts->execute(array($card));
