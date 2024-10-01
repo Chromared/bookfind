293393775a42e150779d2fee6a36788b9fb64c67
MythUp <105284202+MythUp@users.noreply.github.com>
2024-10-01 15:49:13 +0000

    Update 2 files
    - /actions/books/sBooks.php
    - /gestion/actions/books/addEmpruntAction.php

--- gestion/actions/books/addEmpruntAction.php
+++ gestion/actions/books/addEmpruntAction.php
@@ -6,20 +6,23 @@
         $card = $_POST['card'];
         $date = $_POST['date'];
 
-        $checkIfUserAlreadyExists = $bdd->prepare('SELECT carte FROM users WHERE carte = ?');
+        $checkIfUserAlreadyExists = $bdd->prepare('SELECT nb_emprunt_max, nb_emprunt FROM users WHERE carte = ?');
         $checkIfUserAlreadyExists->execute(array($card));
+        $user = $checkIfUserAlreadyExists->fetch();
 
         $checkIfBookAlreadyExists = $bdd->prepare('SELECT id, titre FROM books WHERE id = ?');
         $checkIfBookAlreadyExists->execute(array($book));
         $booksInfos = $checkIfBookAlreadyExists->fetch();
 
 
-        $checkIfBookAlreadyAvailable = $bdd->prepare('SELECT statut FROM emprunts WHERE id_book = ?');
-        $checkIfBookAlreadyAvailable->execute(array($book));
+        $checkIfBookAlreadyAvailable = $bdd->prepare('SELECT statut FROM emprunts WHERE id_book = ? AND statut = ?');
+        $checkIfBookAlreadyAvailable->execute(array($book, 0));
+        $empruntsInfos = $checkIfBookAlreadyAvailable->fetch();
 
-        if($checkIfUserAlreadyExists->rowCount() > 0){    
+        if($checkIfUserAlreadyExists->rowCount() > 0){   
         if($checkIfBookAlreadyExists->rowCount() > 0){
-        if($checkIfBookAlreadyAvailable !== 1){
+        if(NULL){
+        if($user['nb_emprunt'] < $user['nb_emprunt_max']){
 
         //Pour le statut, 1 = emprunté, 2 = retourné, NULL ou autre = erreur -> à corriger
 
@@ -28,7 +31,12 @@
 
         $updateEmpruntForBooks = $bdd->prepare('UPDATE books SET statut = ? WHERE id = ?');
         $updateEmpruntForBooks->execute(array(1, $book));
+        
+        $user_nb_emprunt = $user['nb_emprunt'] +1;
+        $updateEmpruntForBooks = $bdd->prepare('UPDATE users SET nb_emprunt = ? WHERE carte = ?');
+        $updateEmpruntForBooks->execute(array($user_nb_emprunt, $card));
 
+    }else{$msg = 'Cet utilisateur a atteint sa limite d\'emprunt.';}
     }else{$msg = 'Ce livre est déjà emprunté.';}
     }else{$msg = 'Cet id (' . $book . ') ne correspond à aucun livre enregistré.';}
     }else{$msg = 'Ce numéro de carte (' . $card . ') n\'est associé à aucun utilisateur.';}
