f02b1bf699efe33ad53c2d827186f30d2e2d8fc7
Alban Muller <intelligence-artificiel@outlook.fr>
2024-10-16 16:29:59 +0000

    Update 23 files
    - /gestion/modify-user.php
    - /gestion/actions/fonctions/count_data.php
    - /gestion/add-books.php
    - /gestion/add-emprunt.php
    - /gestion/books-reader.php
    - /gestion/books.php
    - /gestion/config-ht.php
    - /gestion/index.php
    - /gestion/log.php
    - /gestion/update-user.php
    - /gestion/users.php
    - /includes/header.php
    - /actions/fonctions/title.js
    - /profil.php
    - /rules.php
    - /signup.php
    - /updateProfil.php
    - /books.php
    - /books-reader.php
    - /emprunts.php
    - /index.php
    - /login.php
    - /pdc.php

--- rules.php
+++ rules.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Règlement</title>
     <?php include 'includes/header.php'; ?>
 </head>
 <body>
