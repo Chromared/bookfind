431b1e86e7ba50baa9650a9f53658e0c5b6468b5
MythUp <105284202+MythUp@users.noreply.github.com>
2024-11-04 17:06:10 +0000

    Update 2 files
    - /actions/fonctions/title.js
    - /gestion/actions/books/returnEmprunt.php

--- actions/fonctions/title.js
+++ actions/fonctions/title.js
@@ -4,7 +4,7 @@ let pageTitleTimeout;
 const startPageTitleFlashing = () => {
     pageTitleTimeout = setInterval(function () {
         document.title = document.title === pageTitle ? "Bookfind" : pageTitle;
-    }, 1000);
+    }, 1500);
 };
 
 window.addEventListener("load",
