8b432fd3534c74951ba742053536708634710053
MythUp <105284202+MythUp@users.noreply.github.com>
2024-11-05 17:23:00 +0000

    Update file sBooks.php

--- actions/books/sBooks.php
+++ actions/books/sBooks.php
@@ -14,8 +14,8 @@
                 <p>Auteur : <?= htmlspecialchars($books['auteur']); ?></p>
                 <p>Résumé : <?php if(!empty($books['resume'])){ echo $books['resume']; }else{ echo 'Il n\'y a pas de résumé pour ce livre.'; } ?></p>
                 <p><a style="color: black;" href="books-reader.php?id=<?= htmlspecialchars($books['id']); ?>">Voir le livre</a></p>
-                <?php if($books['statut'] == 0){ if($_SESSION['grade'] != 0){?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>add-emprunt.php"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><button type="submit" value="validate">Emprunter ce livre</button></form>
-                <?php }}elseif($books['statut'] == 1){ ?><form method="post" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>#"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><button type="submit" value="validate">Retourner cet emprunt</button></form><?php } ?>
+                <?php if($_SESSION['grade'] != 0){ if($books['statut'] == 0){?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>add-emprunt.php"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><button type="submit" value="validate">Emprunter ce livre</button></form>
+                <?php }elseif($books['statut'] == 1){ ?><form method="post" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>#"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><button type="submit" value="validate">Retourner cet emprunt</button></form><?php }} ?>
             </div>
             <br />
             
