a45457a448ef9539ae2c62a9b34a7efbd341eb05
Alban Muller <intelligence-artificiel@outlook.fr>
2024-11-05 16:54:12 +0000

    Update file sBooks.php

--- actions/books/sBooks.php
+++ actions/books/sBooks.php
@@ -14,8 +14,8 @@
                 <p>Auteur : <?= htmlspecialchars($books['auteur']); ?></p>
                 <p>Résumé : <?php if(!empty($books['resume'])){ echo $books['resume']; }else{ echo 'Il n\'y a pas de résumé pour ce livre.'; } ?></p>
                 <p><a style="color: black;" href="books-reader.php?id=<?= htmlspecialchars($books['id']); ?>">Voir le livre</a></p>
-                <?php if($books['statut'] == 0){ if($_SESSION['grade'] != 0){?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>add-emprunt.php"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><input type="submit" value="Ajouter un emprunt"/></form>
-                <?php }}elseif($books['statut'] == 1){ ?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>#"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><input type="submit" value="Rendre ce livre"/></form><?php } ?>
+                <?php if($books['statut'] == 0){ if($_SESSION['grade'] != 0){?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>add-emprunt.php"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><button type="submit" value="validate"></button></form>
+                <?php }}elseif($books['statut'] == 1){ ?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>#"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><button type="submit" value="validate"></button></form><?php } ?>
             </div>
             <br />
             
