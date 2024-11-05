4997a53ac4df0afcbf99424053d3daeb9af13008
MythUp <105284202+MythUp@users.noreply.github.com>
2024-11-05 17:29:05 +0000

    Update 2 files
    - /gestion/actions/books/returnEmprunt.php
    - /actions/books/sBooks.php

--- gestion/actions/books/returnEmprunt.php
+++ gestion/actions/books/returnEmprunt.php
@@ -1,7 +1,9 @@
 <?php if(isset($_POST['validate'])){
     if(isset($_POST['id']) AND !empty($_POST['id'])){
 
-        
+        //Pour le statut, 1 = emprunté, 2 = retourné. NULL ou autre = erreur -> à corriger
+        $updateEmpruntForBooks = $bdd->prepare('UPDATE books SET statut = ? WHERE id = ?');
+        $updateEmpruntForBooks->execute(array(2, $book));
 
     }
 }
