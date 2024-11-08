bee06c02a45c5dfa2b69486e9764fa7f401ac9bd
joacks leloup <joacks.leloup@outlook.fr>
2024-11-08 19:23:03 +0000

    Update 70 files
    - /LICENSE
    - /README.md
    - /login.php
    - /actions/books/sBooks.php
    - /actions/books/showEmprunts.php
    - /actions/books/showOneBooksAction.php
    - /actions/fonctions/conversionDate.php
    - /actions/fonctions/conversionDateHour.php
    - /actions/fonctions/logFonction.php
    - /actions/fonctions/selectedClasse.php
    - /actions/fonctions/selectedGrade.php
    - /actions/fonctions/title.js
    - /actions/fonctions/transfoGradeIntVersText.php
    - /actions/users/deleteAccountAction1.php
    - /actions/users/deleteAccountAction2.php
    - /actions/users/loginAction.php
    - /actions/users/logoutAction.php
    - /actions/users/securityAction.php
    - /actions/users/securityGetIdProfil.php
    - /actions/users/showOneUsersProfilAction.php
    - /actions/users/signupAction.php
    - /actions/users/updateInfoPersoAction.php
    - /actions/users/updateInfoScoAction.php
    - /actions/users/updateMdpAction.php
    - /actions/database.php
    - /gestion/actions/books/addAuthorsAction.php
    - /gestion/actions/books/addBooksAction.php
    - /gestion/actions/books/addEmpruntAction.php
    - /gestion/actions/books/list-author.php
    - /gestion/actions/books/list-editeur.php
    - /gestion/actions/books/list-genre.php
    - /gestion/actions/books/list-serie.php
    - /gestion/actions/books/list-type.php
    - /gestion/actions/books/returnEmprunt.php
    - /gestion/actions/fonctions/count_data.php
    - /gestion/actions/users/deleteAccountAction1.php
    - /gestion/actions/users/deleteAccountAction2.php
    - /gestion/actions/users/sUsers.php
    - /gestion/actions/users/updateGradeAction.php
    - /gestion/actions/users/updateInfoPersoAction.php
    - /gestion/actions/users/updateInfoScoAction.php
    - /gestion/actions/users/updateMdpAction.php
    - /gestion/actions/securityActionAdmin.php
    - /gestion/includes/navbar.php
    - /gestion/add-books.php
    - /gestion/add-emprunt.php
    - /gestion/books-reader.php
    - /gestion/books.php
    - /gestion/config-ht.php
    - /gestion/emprunts.php
    - /gestion/index.php
    - /gestion/log.php
    - /gestion/search-authors.php
    - /gestion/search-editors.php
    - /gestion/search-genres.php
    - /gestion/search-types.php
    - /gestion/update-user.php
    - /gestion/users.php
    - /includes/foot.php
    - /includes/header.php
    - /includes/navbar.php
    - /books-reader.php
    - /books.php
    - /emprunts.php
    - /index.php
    - /pdc.php
    - /profil.php
    - /rules.php
    - /signup.php
    - /updateProfil.php

--- actions/fonctions/conversionDate.php
+++ actions/fonctions/conversionDate.php
@@ -1,5 +1,8 @@
-# X11 License
-# 2024 Chromared
+# This file belongs to the Bookfind project.
+#
+# Bookfind is distributed under the terms of the MIT software license.
+#
+# Copyright (C) 2024 Chromared
 
 
 <?php function ConversionDate($date) {
--- actions/fonctions/logFonction.php
+++ actions/fonctions/logFonction.php
@@ -1,5 +1,8 @@
-# X11 License
-# 2024 Chromared
+# This file belongs to the Bookfind project.
+#
+# Bookfind is distributed under the terms of the MIT software license.
+#
+# Copyright (C) 2024 Chromared
 
 
 <?php function Log($page, $user_id, $user_card, $type, $comment){
