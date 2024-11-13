e8f26247646999a70b2b13682957664ca58de158
MythUp <105284202+MythUp@users.noreply.github.com>
2024-11-13 17:24:48 +0000

    Update file bookfind.sql

---
+++ actions/bookfind.sql
@@ -0,0 +1,325 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 13 nov. 2024 à 18:24
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bookfind`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nomprenom` varchar(255) NOT NULL,
+  `biographie` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `authors`
+--
+
+INSERT INTO `authors` (`id`, `nom`, `prenom`, `nomprenom`, `biographie`) VALUES
+(3, 'Riordans', 'Rick', 'Riordans, Rick', 'Auteur de Percy Jackson'),
+(4, 'Laucoin', 'Joachim', 'Laucoin, Joachim', NULL),
+(5, 'Gaudron', 'Thomas', 'Gaudron, Thomas', NULL),
+(6, 'Muller', 'Alban', 'Muller, Alban', 'Il est le développeur backend de Bookfind. Il est génial !');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `auteur` varchar(255) NOT NULL,
+  `isbn` int(255) NOT NULL,
+  `id_unique` varchar(255) DEFAULT NULL,
+  `resume` text DEFAULT NULL,
+  `editeur` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `serie` varchar(255) DEFAULT NULL,
+  `tome` int(11) DEFAULT NULL,
+  `statut` int(11) NOT NULL,
+  `genre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `books`
+--
+
+INSERT INTO `books` (`id`, `titre`, `auteur`, `isbn`, `id_unique`, `resume`, `editeur`, `type`, `serie`, `tome`, `statut`, `genre`) VALUES
+(1, 'Alban', 'Muller, Alban', 123, '', '', 'WIZ', 'Manuel scolaire', '', 0, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `editeurs`
+--
+
+CREATE TABLE `editeurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `editeurs`
+--
+
+INSERT INTO `editeurs` (`id`, `nom`) VALUES
+(1, 'WIZ'),
+(2, 'Albin Michel');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emprunts`
+--
+
+CREATE TABLE `emprunts` (
+  `id` int(11) NOT NULL,
+  `id_book` int(11) NOT NULL,
+  `titre_book` varchar(255) NOT NULL,
+  `date_emprunt` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_retour` date NOT NULL,
+  `card_emprunteur` int(11) NOT NULL,
+  `firstname_name` varchar(255) NOT NULL,
+  `statut` int(3) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `emprunts`
+--
+
+INSERT INTO `emprunts` (`id`, `id_book`, `titre_book`, `date_emprunt`, `date_retour`, `card_emprunteur`, `firstname_name`, `statut`) VALUES
+(1, 1, 'Alban', '2024-11-07 18:17:35', '2024-12-07', 89702661, 'Alban Muller', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `genres`
+--
+
+INSERT INTO `genres` (`id`, `nom`) VALUES
+(1, 'Horreur'),
+(2, 'Humour'),
+(3, 'Fantasy'),
+(4, 'Fantastique');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL,
+  `page` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_card` int(11) NOT NULL,
+  `browser` varchar(255) NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `os` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `comment` text NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `log`
+--
+
+INSERT INTO `log` (`id`, `page`, `user_id`, `user_card`, `browser`, `version`, `os`, `type`, `comment`, `datetime`) VALUES
+(1, '/bf/login.php', 1, 89702661, 'Default Browser', '0', 'unknown', 'Connexion', 'Aucun', '2024-11-13 18:22:37');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `types`
+--
+
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `types`
+--
+
+INSERT INTO `types` (`id`, `nom`) VALUES
+(1, 'Manuel scolaire'),
+(2, 'Bande dessinées'),
+(4, 'Roman'),
+(5, 'Manga');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `carte` int(8) NOT NULL,
+  `classe` varchar(2) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(25) NOT NULL,
+  `mdp` text NOT NULL,
+  `grade` int(1) NOT NULL DEFAULT 0,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `regles` int(1) NOT NULL,
+  `pdc` int(1) NOT NULL,
+  `nb_emprunt_max` int(255) NOT NULL DEFAULT 5,
+  `nb_emprunt` int(255) NOT NULL DEFAULT 0,
+  `theme` int(2) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `carte`, `classe`, `nom`, `prenom`, `mdp`, `grade`, `datetime`, `regles`, `pdc`, `nb_emprunt_max`, `nb_emprunt`, `theme`) VALUES
+(1, 89702661, '3B', 'Muller', 'Alban', '2yrir8P1qFkn.', 1, '2024-09-27 17:35:04', 1, 1, 5, 1, 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_unique` (`id_unique`);
+
+--
+-- Index pour la table `editeurs`
+--
+ALTER TABLE `editeurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `emprunts`
+--
+ALTER TABLE `emprunts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `carte` (`carte`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `editeurs`
+--
+ALTER TABLE `editeurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `emprunts`
+--
+ALTER TABLE `emprunts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `types`
+--
+ALTER TABLE `types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
