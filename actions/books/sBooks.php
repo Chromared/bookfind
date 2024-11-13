3b922bea792820c87714cfe70f8ae8361313768b
MythUp <105284202+MythUp@users.noreply.github.com>
2024-11-13 13:31:21 +0000

    Update sBooks.php

--- actions/books/sBooks.php
+++ actions/books/sBooks.php
@@ -29,14 +29,14 @@
                 <p>Auteur : <?= htmlspecialchars($books['auteur']); ?></p>
                 <p>Résumé : <?php if(!empty($books['resume'])){ echo $books['resume']; }else{ echo 'Il n\'y a pas de résumé pour ce livre.'; } ?></p>
                 <p><a style="color: black;" href="books-reader.php?id=<?= htmlspecialchars($books['id']); ?>">Voir le livre</a></p>
-                <?php if($_SESSION['grade'] != 0){
+                <?php if(isset($_SESSION['auth'])){ if($_SESSION['grade'] != 0){
                          if($books['statut'] == 0){?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>add-emprunt.php"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><button type="submit" value="validate">Emprunter ce livre</button></form>
                     <?php }elseif($books['statut'] == 1){?>
                 <p>Emprunté par : <?= htmlspecialchars($emprunts['firstname_name']); ?></p>
                 <p>Le : <?php ConversionDateHour($emprunts['date_emprunt']); ?></p>
                 <p>Retour prévu le : <?php ConversionDate($emprunts['date_retour']); ?></p>
                         <form method="post" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>#"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><input type="hidden" name="card" value="<?= htmlspecialchars($emprunts['card_emprunteur']); ?>"/><button type="submit" value="validate">Retourner cet emprunt</button></form>
-                <?php }} ?>
+                <?php }}} ?>
             </div>
             <br />
             
