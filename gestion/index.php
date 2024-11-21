76e3fe1e23719e703324882cbd58eb6652bf982b
MythUp <105284202+MythUp@users.noreply.github.com>
2024-11-21 10:55:41 +0000

    Update index.php

--- gestion/index.php
+++ gestion/index.php
@@ -35,7 +35,7 @@ require 'actions/securityActionAdmin.php';
         <i class="fa-solid fa-user"></i> <span id="utilisateurs"><i class="fa-duotone fa-solid fa-spinner fa-spin-pulse"></i></span><br>
         <i class="fa-solid fa-book"></i> <span id="livres"><i class="fa-duotone fa-solid fa-spinner fa-spin-pulse"></i></span><br>
         <i class="fa-solid fa-upload"></i> <span id="emprunts"><i class="fa-duotone fa-solid fa-spinner fa-spin-pulse"></i></span><br>
-        <i class="fa-solid fa-newspaper"> <span id="log"><i class="fa-duotone fa-solid fa-spinner fa-spin-pulse"></i></span></i>
+        <i class="fa-solid fa-newspaper"> <span id="log"><i class="fa-duotone fa-solid fa-spinner fa-spin-pulse"></i></span></i><br />
     </p>
 
     <script>
