368da08d17cf631576e98b5928c1486149d1d997
AMu11er <alban.l.muller@outlook.com>
2024-12-04 18:49:13 +0100

    Update 5 files and add 2 files

--- actions/books/sBooks.php
+++ actions/books/sBooks.php
@@ -30,12 +30,23 @@
                 <p>Résumé : <?php if(!empty($books['resume'])){ echo $books['resume']; }else{ echo 'Il n\'y a pas de résumé pour ce livre.'; } ?></p>
                 <p><a style="color: black;" href="books-reader.php?id=<?= htmlspecialchars($books['id']); ?>">Voir le livre</a></p>
                 <?php if(isset($_SESSION['auth'])){ if($_SESSION['grade'] != 0){
-                         if($books['statut'] == 0){?><form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>add-emprunt.php"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><input type="submit" name="validate" value="Emprunter ce livre"/></form>
-                    <?php }elseif($books['statut'] == 1){?>
-                <p>Emprunté par : <?= htmlspecialchars($emprunts['firstname_name']); ?></p>
-                <p>Le : <?php ConversionDateHour($emprunts['date_emprunt']); ?></p>
-                <p>Retour prévu le : <?php ConversionDate($emprunts['date_retour']); ?></p>
-                        <form method="post" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>#"><input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/><input type="hidden" name="card" value="<?= htmlspecialchars($emprunts['card_emprunteur']); ?>"/><input type="submit" name="validate" value="Retourner cet emprunt"/></form>
+                         if($books['statut'] == 0){?>
+
+                            <form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>add-emprunt.php">
+                                <input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/>
+                                <input type="submit" value="Emprunter ce livre"/>
+                            </form>
+                    
+                        <?php }elseif($books['statut'] == 1){?>
+
+                            <p>Emprunté par : <?= htmlspecialchars($emprunts['firstname_name']); ?></p>
+                            <p>Le : <?php ConversionDateHour($emprunts['date_emprunt']); ?></p>
+                            <p>Retour prévu le : <?php ConversionDate($emprunts['date_retour']); ?></p>
+                            <form method="get" action="<?php if(!isset($gestion)){ ?>gestion/<?php } ?>update-emprunt.php">
+                                <input type="hidden" name="id" value="<?= htmlspecialchars($books['id']); ?>"/>
+                                <input type="hidden" name="card" value="<?= htmlspecialchars($emprunts['card_emprunteur']); ?>"/>
+                                <input type="submit" value="Modifier l'emprunt de ce livre"/>
+                            </form>
                 <?php }}} ?>
             </div>
             <br />
--- gestion/update-emprunt.php
+++ gestion/update-emprunt.php
@@ -8,7 +8,8 @@
 
 <?php require '../actions/database.php';
     require '../actions/users/securityAction.php';
-    require 'actions/securityActionAdmin.php'; ?>
+    require 'actions/securityActionAdmin.php';
+    require 'actions/books/showOneEmprunt.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -20,6 +21,20 @@
 <body>
     <?php include 'includes/navbar.php'; ?>
     <br>
-    
+    <div class="update-part">
+        <h4>Modifier la date de retour de l'emprunt</h4>
+        <form method="get">
+        <input type="date" name="date" value="<?= $empruntInfos['date_retour'] ?>" /><br />
+        <input type="submit" name="validateUpdate" value="Enregistrer" />
+        </form>
+    </div>
+    <hr />
+    <div class="update-part">
+        <form method="post">
+            <input type="hidden" name="id" value="<?= $_GET['id'] ?>" />
+            <input type="hidden" name="card" value="<?= $_GET['card'] ?>" />
+            <input type="submit" name="validateReturn" value="Retourner l'emprunt" />
+        </form>
+    </div>
 </body>
 </html>
