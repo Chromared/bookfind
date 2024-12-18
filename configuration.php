24e0484ea72ea38f39f198d67c1d52dccf3fa09f
Alban Muller <alban.l.muller@outlook.com>
2024-12-18 11:29:32 +0000

    Update 10 files
    - /configuration.php
    - /.vscode/settings.json
    - /actions/bookfind.sql
    - /actions/fonctions/recupClassesAndOptions.php
    - /actions/users/showOneUsersProfilAction.php
    - /gestion/emprunt.php
    - /gestion/logs.php
    - /gestion/actions/books/showOneEmprunt.php
    - /gestion/actions/fonctions/loadLogs.php
    - /gestion/actions/users/securityAction.php

--- configuration.php
+++ configuration.php
@@ -27,9 +27,6 @@
                         <label for="host">Hôte :</label>
                         <input type="text" id="host" name="host" required /><br><br>
 
-                        <label for="dbname">Nom de la Base de Données :</label>
-                        <input type="text" id="dbname" name="dbname" required /><br><br>
-
                         <label for="username">Nom d'utilisateur :</label>
                         <input type="text" id="username" name="username" required /><br><br>
 
@@ -39,9 +36,8 @@
                         <input type="submit" value="Enregistrer" />
                 </form>
                 </div>
-                                <?php if (isset($_POST['host']) AND isset($_POST['dbname']) AND isset($_POST['username']) AND isset($_POST['password']) AND !empty($_POST['host']) AND !empty($_POST['dbname']) AND !empty($_POST['username'])) {
+                                <?php if (isset($_POST['host']) AND isset($_POST['username']) AND isset($_POST['password']) AND !empty($_POST['host']) AND !empty($_POST['username'])) {
                                     $newHost = $_POST['host'];
-                                    $newDBname = $_POST['dbname'];
                                     $newUsername = $_POST['username'];
                                     $newPassword = $_POST['password'];
                                 
@@ -50,7 +46,6 @@
                                     $fileContent = file_get_contents($filePath);
                                 
                                     $fileContent = preg_replace("/\\\$host = '';/", "\$host = '$newHost';", $fileContent);
-                                    $fileContent = preg_replace("/\\\$dbname = '';/", "\$dbname = '$newDBname';", $fileContent);
                                     $fileContent = preg_replace("/\\\$username = '';/", "\$username = '$newUsername';", $fileContent);
                                     $fileContent = preg_replace("/\\\$password = '';/", "\$password = '$newPassword';", $fileContent);
                                 
@@ -62,10 +57,10 @@
                 <?php }else{ echo 'Étape 1 validée.'; $step1 = true; } ?></p>
 
                 <?php if (file_exists('actions/bookfind.sql')) { ?>
-                <p><strong>2. Configurer la base de donnée :</strong> En cliquant sur ce bouton, la base de donnée va être importé sur le serveur : <form method="submit"><input type="submit" name="import" value="Importer la base de donnée" /></form></p>
+                <p><strong>2. Configurer la base de donnée :</strong> En cliquant sur ce bouton, la base de donnée va être importé sur le serveur : <form method="post"><input type="submit" name="import" value="Importer la base de donnée" /></form>
                 
                 <?php if(isset($_POST['import'])){
-                    if(!empty($host) AND !empty($dbname) AND !empty($username)){
+                    if(!empty($host) AND !empty($username)){
                     $sqlFile = 'actions/bookfind.sql';
 
                     $sql = file_get_contents($sqlFile);
@@ -73,6 +68,13 @@
                     if ($sql === false) {
                         die("Impossible de lire le fichier SQL.");
                     }
+
+                    $createDatabase = $bdd->exec('CREATE DATABASE bookfind');
+
+                    $filePath = 'actions/database.php';
+                    $fileContent = file_get_contents($filePath);
+                    $fileContent = preg_replace("/\\\$dbname = '';/", "\$dbname = 'bookfind';", $fileContent);
+                    file_put_contents($filePath, $fileContent);
 
                     $queries = array_filter(array_map('trim', explode(';', $sql)));
 
@@ -87,7 +89,8 @@
                     echo 'Importation de la base de donnée terminée avec succès. Rechargez la page <a href="configuration.php">ici</a>.';
                 }else{ echo 'Les identifiants de la base de donnée n\'ont pas été indiqués. Veuillez le faire à l\'aide du <a href="#db">formulaire</a> plus haut.'; }}
             }else{ echo 'Étape 2 validée.'; $step2 = true; } ?>
-                    <br />
+                    <br /></p>
+
                 <p><strong>3. Création des classes :</strong> lors de l'inscription d'un utilisateur, sa classe lui est demandée (Ex : 6B, 5A...). Dans ce formulaire, vous allez pouvoir créer autant de classe que vous voulez. Attention : la classe sert juste à caractériser un utilisateur. Elle n'influe pas sur le grade et ne donne pas de permissions. Voici le formulaire :<br />
             
                 <div class="update-part">
@@ -110,9 +113,9 @@
                         $addClasse = $bdd->prepare('INSERT INTO classes SET name = ?');
                         $addClasse->execute(array($classe));
 
-                        echo 'Classe ajoutée avec succès.';
+                        echo 'Classe ajoutée avec succès.<br />';
 
-                    }else{ echo 'Cette classe existe déjà.'; }
+                    }else{ echo 'Cette classe existe déjà. <br />'; }
                     
                 }else{ echo 'Les identifiants de la base de donnée n\'ont pas été indiqués. Veuillez le faire à l\'aide du <a href="#db">formulaire</a> plus haut.'; }
             } if(!empty($host) AND !empty($dbname) AND !empty($username)){
@@ -123,7 +126,7 @@
                     $checkIfOneUserExist = $bdd->query('SELECT id FROM users');
                                     if($checkIfOneUserExist->rowCount() == 0){ ?>
 
-                <p><strong>4. Création d'un compte administrateur :</strong> vous pouvez créer le premier compte, qui sera automatiquement gradé en tant que "administrateur". Pour cela, rendez-vous sur la page d'<a href="signin.php" target="_blank">inscription</a> pour vous inscrire.</p>
+                <p><strong>4. Création d'un compte administrateur :</strong> vous pouvez créer le premier compte, qui sera automatiquement gradé en tant que "administrateur". Pour cela, rendez-vous sur la page d'<a href="signup.php" target="_blank">inscription</a> pour vous inscrire.</p>
             <?php }else{ echo 'Étape 4 validée.'; $step4 = true; }} ?>
 
             <?php if(isset($step1) AND isset($step2) AND isset($step3) AND isset( $step4)){
@@ -134,6 +137,9 @@
                     <input type="submit" name="delete" value="Supprimer ce fichier" />
                 </form>
 
-            <?php  if(isset($_POST['delete'])){  }}} ?>
+            <?php  if(isset($_POST['delete'])){ 
+                unlink('configuration.php');
+                echo 'Page supprimée. Vous pouvez fermer la page ou vous rendre à l\'accueil en cliquant <a href="index.php">ici</a>.';
+             }}} ?>
 </body>
 </html>
