2a3fcb97fac1d6fdd7f720baf4d16b69269f5c1c
Alban Muller <alban.l.muller@outlook.com>
2024-12-18 18:03:49 +0000

    Update 2 files
    - /gestion/add-books.php
    - /style/style0.css

--- gestion/add-books.php
+++ gestion/add-books.php
@@ -22,21 +22,22 @@
 <?php include 'includes/navbar.php'; ?>
 <br />
 <p>Les champs marqués d'une * sont obligatoires.</p>
+<p>Vous pouvez utiliser <em>CTRL + ENTER</em> ou <em>CMD + ENTER</em> pour valider.</p>
 <?php if(isset($msg)){echo '<p>' . $msg . '</p>';}
     if(isset($_GET['msg']) AND !empty($_GET['msg']) AND $_GET['msg'] == true){echo '<p>Le livre a bien été enregistré.</p>';}?>
 <br />
 <div class="update-part"><h4>Ajouter un livre</h4>
 <form method="post" autocomplete="off">
-<label for="title">Titre* : </label><input type="text" id="title" name="title" required/><br />
-<label for="author">Auteur* : </label><input type="text" id="author" name="author" required/><span class="list-author"></span><?php include 'actions/books/list-author.php'; ?><br />
-<label for="isbn">ISBN* : </label><input type="number" id="isbn" name="isbn" required/><br />
-<label for="type">Type* : </label><input type="text" id="type" name="type" required/><span class="list-type"></span><?php include 'actions/books/list-type.php'; ?><br />
-<label for="editeur">Éditeur* : </label><input type="text" itemid="editeur" name="editeur" id="editeur" required/><span class="list-editeur"></span><?php include 'actions/books/list-editeur.php'; ?><br />
-<label for="resume">Résumé : </label><textarea id="resume" name="resume"></textarea><br />
-<label for="id_u">Identifiant unique : </label><input type="text" id="id_u" name="id_unique"/><br />
-<label for="genre">Genre : </label><input type="text" id="genre" name="genre" /><span class="list-genre"></span><?php include 'actions/books/list-genre.php'; ?><br />
-<label for="serie">Série : </label><input type="text" id="serie" name="serie" /><span class="list-serie"></span><?php include 'actions/books/list-serie.php'; ?><br />
-<label for="tome">Tome : </label><input type="number" id="tome" name="tome" /><br />
+<input placeholder="Titre*" type="text" id="title" name="title" required/><br />
+<input placeholder="Auteur*" type="text" id="author" name="author" required/><span class="list-author"></span><?php include 'actions/books/list-author.php'; ?><br />
+<input placeholder="ISBN*" type="number" id="isbn" name="isbn" required/><br />
+<input placeholder="Type*" type="text" id="type" name="type" required/><span class="list-type"></span><?php include 'actions/books/list-type.php'; ?><br />
+<input placeholder="Éditeur*" type="text" itemid="editeur" name="editeur" id="editeur" required/><span class="list-editeur"></span><?php include 'actions/books/list-editeur.php'; ?><br />
+<textarea placeholder="Résumé" id="resume" name="resume"></textarea><br />
+<input placeholder="Identifiant unique" type="text" id="id_u" name="id_unique"/><br />
+<input placeholder="Genre" type="text" id="genre" name="genre" /><span class="list-genre"></span><?php include 'actions/books/list-genre.php'; ?><br />
+<input placeholder="Série" type="text" id="serie" name="serie" /><span class="list-serie"></span><?php include 'actions/books/list-serie.php'; ?><br />
+<input placeholder="Tome (n°)" type="number" id="tome" name="tome" /><br />
 <input type="hidden" name="validate" />
 <input type="submit" name="validate" title="Ctrl + Enter / Cmd + Enter" value="Enregistrer"/>
 
