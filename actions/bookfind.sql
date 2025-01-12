b89b84b3e45ee488ba8d7745662147a433dcb56c
MythUp <105284202+MythUp@users.noreply.github.com>
2025-01-12 18:22:51 +0000

    Update 2 files

---
+++ actions/bookfind.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 05 jan. 2025 à 14:23
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bookfind`
+--
+CREATE DATABASE IF NOT EXISTS `bookfind` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bookfind`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `auteur` varchar(255) NOT NULL,
+  `isbn` int(255) NOT NULL,
+  `id_unique` varchar(255) DEFAULT NULL,
+  `resume` text DEFAULT NULL,
+  `editeur` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `serie` varchar(255) DEFAULT NULL,
+  `tome` int(11) DEFAULT NULL,
+  `statut` int(11) NOT NULL,
+  `genre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `classes`
+--
+
+INSERT INTO `classes` (`id`, `name`) VALUES
+(1, 'Aucune');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emprunts`
+--
+
+CREATE TABLE `emprunts` (
+  `id` int(11) NOT NULL,
+  `id_book` int(11) NOT NULL,
+  `titre_book` varchar(255) NOT NULL,
+  `date_emprunt` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_futur_retour` date NOT NULL,
+  `date_retour` datetime DEFAULT NULL,
+  `card_emprunteur` int(11) NOT NULL,
+  `firstname_name` varchar(255) NOT NULL,
+  `statut` int(3) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `page` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_card` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `comment` text NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `carte` int(8) NOT NULL,
+  `classe` varchar(255) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(25) NOT NULL,
+  `mdp` text NOT NULL,
+  `grade` int(1) NOT NULL DEFAULT 0,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `regles` int(1) NOT NULL,
+  `pdc` int(1) NOT NULL,
+  `nb_emprunt_max` int(255) NOT NULL DEFAULT 5,
+  `nb_emprunt` int(255) NOT NULL DEFAULT 0,
+  `theme` int(2) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Index pour la table `emprunts`
+--
+ALTER TABLE `emprunts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `carte` (`carte`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `emprunts`
+--
+ALTER TABLE `emprunts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ gestion/emprunt.php
@@ -0,0 +1,60 @@
+<?php
+//This file belongs to the Bookfind project.
+//
+//Bookfind is distributed under the terms of the MIT software license.
+//
+//Copyright (C) 2024 Chromared
+?>
+
+<?php require '../actions/database.php';
+    require 'actions/users/securityAction.php';
+    require 'actions/securityActionAdmin.php';
+    require '../actions/books/showOneBooksAction.php';
+    require 'actions/books/updateEmprunt.php';
+    if($booksInfos['statut'] == 1){ require 'actions/books/showOneEmprunt.php'; }
+    require 'actions/books/addEmpruntAction.php';
+    require 'actions/books/returnEmprunt.php';
+
+    $dateDans30Jours = date('Y-m-d', strtotime('+30 days')); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier un emprunt</title>
+    <?php include '../includes/header.php'; ?>
+</head>
+<body>
+    <?php include 'includes/navbar.php'; ?>
+    <br>
+    <?php if(isset($msg)){$msg;}
+        if($booksInfos['statut'] == 0 OR $booksInfos['statut'] == 2){ ?>
+    <h4>Ajouter un emprunt</h4>
+    <form method="post">
+        <label>N° de carte de l'emprunteur : </label><input type="number" name="card" min="0" max="99999999"/><br />
+        <label>À rendre pour le : </label><input type="date" value="<?= $dateDans30Jours; ?>" name="date" /><br />
+        <input type="hidden" name="id_book" value="<?= htmlspecialchars($_GET['id']); ?>" />
+        <input type="submit" name="validateAdd" value="Ajouter l'emprunt"/>
+    </form>
+        <?php }elseif($booksInfos['statut'] == 1){
+            if(isset($_GET['card']) AND !empty($_GET['card'])){ ?>
+    <div class="update-part">
+        <h4>Modifier la date de retour de l'emprunt</h4>
+        <form method="post">
+        <input type="date" name="date" value="<?= $empruntInfos['date_futur_retour'] ?>" /><br />
+        <input type="hidden" name="id" value="<?= $_GET['id'] ?>" />
+            <input type="hidden" name="card" value="<?= $_GET['card'] ?>" />
+        <input type="submit" name="validateUpdate" value="Enregistrer" />
+        </form>
+    </div>
+    <hr />
+    <div class="update-part">
+        <form method="post">
+            <input type="hidden" name="id" value="<?= $_GET['id'] ?>" />
+            <input type="hidden" name="card" value="<?= $_GET['card'] ?>" />
+            <input type="submit" name="validateReturn" value="Retourner l'emprunt" />
+        </form>
+    </div>
+<?php }else{echo 'N° de carte de l\'utilisateur manquant.';}} ?>
+</body>
+</html>
