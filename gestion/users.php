37e100e682d8ce79f44ac5ed2c86ec452344f92e
MythUp <105284202+MythUp@users.noreply.github.com>
2025-03-24 18:49:45 +0100

    Update 2 files

--- gestion/users.php
+++ gestion/users.php
@@ -18,13 +18,10 @@
     <?php include '../includes/header.php'; ?>
 </head>
 <body>
-    <?php include 'includes/navbar.php'; 
-    if(isset($_GET['where']) AND !empty($_GET['where'])){ $getwhere = $_GET['where'];}else{$getwhere = NULL;}
-    function Selected($where, $value){if(isset($where) AND !empty($where) AND $where == $value){echo htmlspecialchars('selected');}} ?>
+    <?php include 'includes/navbar.php'; ?>
         <div class="usersgestion-part">
     <form method="GET" class="form">
         <br /><input type="search" name="s" value="<?php if(isset($_GET['s']) AND !empty($_GET['s'])){echo htmlspecialchars($_GET['s']);} ?>" placeholder="Rechercher un utilisateur" class="form-control" /> 
-        <select name="where"><option <?php Selected($getwhere, 'carte'); ?> value="carte">Carte</option><option <?php Selected($getwhere, 'id'); ?> value="id">ID</option><option <?php Selected($getwhere, 'classe'); ?> value="classe">Classe</option><option <?php Selected($getwhere, 'nom'); ?> value="nom">Nom</option><option <?php Selected($getwhere, 'prenom'); ?> value="prenom">Prénom</option><option <?php Selected($getwhere, 'grade'); ?> value="grade">Grade</option></select>
         <button class="btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
     </form>
     <?php include 'actions/users/sUsers.php'; ?>
