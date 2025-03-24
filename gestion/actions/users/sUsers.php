6046ff599239222550c6cc325ce70de76795261b
MythUp <105284202+MythUp@users.noreply.github.com>
2025-03-24 18:49:45 +0100

    Update 2 files

--- gestion/actions/users/sUsers.php
+++ gestion/actions/users/sUsers.php
@@ -10,23 +10,25 @@
 
 <?php include '../actions/functions/transfoGradeIntVersText.php';
 
-$colonnes_valides = ['id', 'carte', 'classe', 'nom', 'prenom', 'grade', 'nb_emprunt_max', 'nb_emprunt'];
+    if(isset($_GET['s']) AND !empty($_GET['s'])) {
 
-if (isset($_GET['s']) && !empty($_GET['s']) && isset($_GET['where']) && !empty($_GET['where'])) {
+        $s = $_GET['s'];
+        $keywords = explode(' ', $s);
+        $searchConditions = [];
 
-    $s = $_GET['s'];
-    $s = "%" . $s . "%";
-    $where = $_GET['where'];
-    
-    if (in_array($where, $colonnes_valides)) {
+        foreach ($keywords as $keyword) {
+            $searchConditions[] = "(prenom LIKE ? OR nom LIKE ? OR carte LIKE ? OR classe LIKE ? OR nb_emprunt LIKE ? OR grade LIKE ?)";
+        }
+
+        $sql = 'SELECT * FROM users WHERE ' . implode(' AND ', $searchConditions) . '';
+        $sUsers = $bdd->prepare($sql);
+        $params = [];
+        foreach ($keywords as $keyword) {
+            $params = array_merge($params, array_fill(0, 6, "%$keyword%"));
+        }
+        $sUsers->execute($params);
         
-        $requete = "SELECT * FROM users WHERE $where LIKE :value ORDER BY $where ASC";
-        $SearchUser = $bdd->prepare($requete);
-        
-        $SearchUser->bindParam(':value', $s, PDO::PARAM_STR);
-        $SearchUser->execute();
-        
-        while ($users = $SearchUser->fetch()) { ?>
+        while ($users = $sUsers->fetch()) { ?>
             <div class="bordure" class="profil-part">
                 <h4><?= htmlspecialchars($users['prenom']); ?> <?= htmlspecialchars($users['nom']); ?></h4>
                 <p>ID : <?= htmlspecialchars($users['id']); ?></p>
@@ -40,10 +42,4 @@ if (isset($_GET['s']) && !empty($_GET['s']) && isset($_GET['where']) && !empty($
                 </div>
             </div>
             <br />
-        <?php }
-        
-    } else {
-        echo "Erreur : Colonne non valide.";
-    }
-    
-}
+        <?php }}
