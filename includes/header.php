3cd6d3c88951548e6f7140e668f764af68001437
Alban Muller <alban.l.muller@outlook.com>
2025-03-31 18:59:26 +0200

    Update 6 files and rename 1 file

--- includes/header.php
+++ includes/header.php
@@ -8,14 +8,17 @@
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<link rel="icon" href="style/img/iconesite.ico" />
-<link rel="icon" href="../style/img/iconesite.ico" />
-<script src="https://kit.fontawesome.com/36a4c41322.js" crossorigin="anonymous"></script>
+<!-- css -->
 <link href="style/style0.css" rel="stylesheet">
 <link href="../style/style0.css" rel="stylesheet">
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<!-- icon -->
+<link rel="icon" href="style/img/favicon.ico" />
+<link rel="icon" href="../style/img/favicon.ico" />
+<!-- script -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script src="https://kit.fontawesome.com/36a4c41322.js" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script src="actions/functions/title.js"></script>
