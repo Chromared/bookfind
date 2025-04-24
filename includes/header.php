a58f4a565beba80e2f9e3fb6d6be0bf4a4684756
Alban Muller <alban.l.muller@outlook.com>
2025-04-24 16:08:14 +0200

    Update files

--- includes/header.php
+++ includes/header.php
@@ -13,8 +13,8 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <!-- icon -->
-<link rel="icon" href="style/img/favicon.ico" />
-<link rel="icon" href="../style/img/favicon.ico" />
+<link rel="icon" href="style/favicon.ico" />
+<link rel="icon" href="../style/favicon.ico" />
 <!-- script -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/36a4c41322.js" crossorigin="anonymous"></script>
