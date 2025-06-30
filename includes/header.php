cbdccf46b376ffebf408a15203c7b331235c3928
MythUp <105284202+MythUp@users.noreply.github.com>
2025-06-30 11:55:08 +0200

    Add 1 file, rename 4 files, update 6 files and delete 1 file

--- includes/header.php
+++ includes/header.php
@@ -9,14 +9,14 @@
 
 
 <!-- css -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<!-- icon -->
-<link rel="icon" href="style/favicon.ico" type="image/x-icon"/>
-<link rel="icon" href="../style/favicon.ico" type="image/x-icon"/>
-<!-- script -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.1/themes/base/jquery-ui.css">
+<!-- icons -->
+<link rel="icon" href="assets/favicon.ico" type="image/x-icon"/>
+<link rel="icon" href="../assets/favicon.ico" type="image/x-icon"/>
+<!-- scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="actions/functions/title.js"></script>
 <script src="../actions/functions/title.js"></script>
