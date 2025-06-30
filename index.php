efda2c80cdd671e354c0b78801f70248a83801d7
MythUp <105284202+MythUp@users.noreply.github.com>
2025-06-30 11:55:08 +0200

    Add 1 file, rename 4 files, update 6 files and delete 1 file

--- index.php
+++ index.php
@@ -16,21 +16,22 @@
     <?php include 'includes/header.php'; ?>
 </head>
 <body>
-    <?php include 'includes/navbar.php'; ?>
-    <div class="container text-center mt-5">
-      <img src="style/iconesite.png" class="img-fluid w-25 display-4" />
-      <h1 class="mt-3">Bienvenue sur BookFind !</h1>
-    </div>
-    <div class="container mt-3">
-        <form method="GET" action="books.php">
-            <div class="input-group mb-3">
-              <input type="text" name="s" class="form-control" value="<?php if(isset($_GET['s']) AND !empty($_GET['s'])){echo htmlspecialchars($_GET['s']);} ?>" placeholder="Rechercher un livre" />
-              <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
-                <i class="bi bi-search"></i>
-                Rechercher
-              </button>
-            </div>
-        </form>
-    </div>
+  <?php include 'includes/navbar.php'; ?>
+  <div class="container text-center mt-5">
+    <img src="assets/iconesite.png" class="img-fluid w-25 display-4" />
+    <h1 class="mt-3">Bienvenue sur BookFind !</h1>
+  </div>
+  <div class="container mt-3">
+      <form method="GET" action="books.php">
+          <div class="input-group mb-3">
+            <input type="text" name="s" class="form-control" value="<?php if(isset($_GET['s']) AND !empty($_GET['s'])){echo htmlspecialchars($_GET['s']);} ?>" placeholder="Rechercher un livre" />
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
+              <i class="bi bi-search"></i>
+              Rechercher
+            </button>
+          </div>
+      </form>
+  </div>
+  <?php include 'includes/footer.php'; ?>
 </body>
 </html>
