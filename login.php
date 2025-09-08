196f7f07a244dd2d860ef64ce983a782c8f0b3f8
MythUp <105284202+MythUp@users.noreply.github.com>
2025-09-08 18:45:17 +0200

    Update 7 files

--- login.php
+++ login.php
@@ -33,11 +33,14 @@
                 </div>
               </div>
             <?php } ?>
+            <h5 class="card-title">Connexion</h5>
             <div class="mb-3">
-              <input type="text" name="username" class="form-control" placeholder="Nom d'utilisateur" autofocus required/>
+              <label for="username" class="form-label text-start d-block">Nom d'utilisateur</label>
+              <input type="text" name="username" id="username" class="form-control" placeholder="jdoe" autofocus required/>
             </div>
             <div class="mb-3">
-              <input type="password" name="password" class="form-control" placeholder="Mot de passe" required/>
+              <label for="password" class="form-label text-start d-block">Mot de passe</label>
+              <input type="password" name="password" id="password" class="form-control" required/>
             </div>
             <div class="mb-3">
               <input type="submit" name="validate" class="btn btn-primary" value="Connexion" />
