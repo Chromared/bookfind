8b2d7b0db873091338f512d1761a79a73accd6d8
TagSteel <183854864+TagSteel@users.noreply.github.com>
2025-12-18 12:11:40 +0100

    Update rules.php for privacy policy page

--- rules.php
+++ rules.php
@@ -6,17 +6,19 @@
 //Copyright (C) 2025 Chromared
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Règlement</title>
+    <title>Politique de confidentialité</title>
     <?php include 'includes/header.php'; ?>
+</head>
+<body>
+<?php include 'includes/navbar.php'; ?>
 </head>
 <body>
     <h1>Coming soon !</h1>
 </body>
+
 </html>
