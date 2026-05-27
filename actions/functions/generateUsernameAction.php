60030e0588d2d8f15b5f81daff8b3412f955f804
MythUp <105284202+MythUp@users.noreply.github.com>
2026-05-27 18:36:52 +0200

    Update the username generation function to remove accents

--- actions/functions/generateUsernameAction.php
+++ actions/functions/generateUsernameAction.php
@@ -1,13 +1,18 @@
-<?php function username($firstname, $lastname){
-    $username = strtolower(substr($firstname, 0, 1) . substr($lastname, 0, 7));
-        // Delete the non-leters caracters (UTF-8 compliant)
-        $cleanFirst = preg_replace('/[^\p{L}\p{N}]/u', '', $firstname);
-        $cleanLast  = preg_replace('/[^\p{L}\p{N}]/u', '', $lastname);
+<?php
+function username($firstname, $lastname) {
+    $clean = function($s) {
+        $s = (string)$s;
+        if (class_exists('Normalizer')) {
+            $s = Normalizer::normalize($s, Normalizer::FORM_D);
+            $s = preg_replace('/\p{M}/u', '', $s);
+        } else {
+            $s = @iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $s) ?: $s;
+        }
+        $s = strtr($s, ['œ'=>'oe','Œ'=>'OE','æ'=>'ae','Æ'=>'AE','ç'=>'c','Ç'=>'C']);
+        return preg_replace('/[^\p{L}\p{N}]/u', '', $s);
+    };
 
-        // Take the first letter of the first name and up to 7 characters of the last name (UTF-8 compliant)
-        $firstChar = mb_substr($cleanFirst, 0, 1, 'UTF-8');
-        $lastPart  = mb_substr($cleanLast, 0, 7, 'UTF-8');
-
-        // Return in lowercase (UTF-8)
-        return mb_strtolower($firstChar . $lastPart, 'UTF-8');
+    $first = $clean($firstname);
+    $last  = $clean($lastname);
+    return mb_strtolower(mb_substr($first, 0, 1, 'UTF-8') . mb_substr($last, 0, 7, 'UTF-8'), 'UTF-8');
 }
