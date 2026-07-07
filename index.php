54fafe89c1d718191781b7a1b64775cbb35e9cf4
MythUp <105284202+MythUp@users.noreply.github.com>
2026-07-07 20:14:30 +0200

    Add aria-label to search field in index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   <div class="container mt-3">
     <form method="GET" action="books.php">
       <div class="input-group mb-3">
-        <input type="text" name="s" class="form-control" placeholder="Rechercher un livre" />
+        <input type="text" name="s" class="form-control" placeholder="Rechercher un livre" aria-label="Rechercher un livre"/>
         <button class="btn btn-outline-secondary" type="submit" id="button-addon2">
           <i class="bi bi-search"></i>
           Rechercher
